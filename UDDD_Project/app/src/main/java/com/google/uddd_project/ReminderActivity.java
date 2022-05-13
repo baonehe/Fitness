bc4ee3507deaa4a50224c1df3187435340d8f2f9
Hoàng Nhật <91327134+HoangNhat212@users.noreply.github.com>
2022-05-14 02:05:06 +0700

    Update Reminder

--- UDDD_Project/app/src/main/java/com/google/uddd_project/ReminderActivity.java
+++ UDDD_Project/app/src/main/java/com/google/uddd_project/ReminderActivity.java
@@ -2,7 +2,9 @@ package com.google.uddd_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ReminderActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ReminderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+
+        findViewById(R.id.button_plus_medication).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MedicationReminderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
