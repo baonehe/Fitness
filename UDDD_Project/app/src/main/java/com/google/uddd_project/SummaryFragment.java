f5b0a44049ad2055d5ba845f7a189623dfe6f96d
Atula <20520994@gm.uit.edu.vn>
2022-05-25 14:44:25 +0700

    Update Maps

--- UDDD_Project/app/src/main/java/com/google/uddd_project/SummaryFragment.java
+++ UDDD_Project/app/src/main/java/com/google/uddd_project/SummaryFragment.java
@@ -1,5 +1,6 @@
 package com.google.uddd_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -59,6 +60,14 @@ public class SummaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary, container, false);
+        View view = inflater.inflate(R.layout.fragment_summary, container, false);
+        view.findViewById(R.id.imgRun).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        return  view;
     }
 }
