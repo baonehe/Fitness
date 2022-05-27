2781143cf86e36cc3a498544944944348257b35e
baonehe <95235910+baonehe@users.noreply.github.com>
2022-05-27 19:22:25 +0700

    Connect realtime and my account

--- UDDD_Project/app/src/main/java/com/google/uddd_project/MyAccountFragment.java
+++ UDDD_Project/app/src/main/java/com/google/uddd_project/MyAccountFragment.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Locale;
 
 
 /**
@@ -55,12 +62,42 @@ public class MyAccountFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    EditText firstname,lastname,city,country,address,age,height,weight,email;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_my_account, container, false);
+        firstname = view.findViewById(R.id.edtfirstname);
+        lastname= view.findViewById(R.id.edtlastname);
+        city = view.findViewById(R.id.edtcity);
+        country= view.findViewById(R.id.edtcountry);
+        address= view.findViewById(R.id.edtaddress);
+        age= view.findViewById(R.id.edtage);
+        height=view.findViewById(R.id.edtheight);
+        weight=view.findViewById(R.id.edtweight);
+        email= view.findViewById(R.id.edtemail);
+        String username= FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        String iduser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        email.setText(username);
+        view.findViewById(R.id.btnsavechange).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if((firstname.getText().length() == 0) ||  (lastname.getText().length() == 0) || (address.getText().length() == 0) || (city.getText().length() == 0) || (country.getText().length() == 0)
+                || (age.getText().length() == 0) || (height.getText().length() == 0) || (weight.getText().length() == 0))
+                    Toast.makeText(view.getContext(), "Please fill full your inFormation", Toast.LENGTH_SHORT).show();
+                else{
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference(iduser);
+                    AccountInfo accountInfo= new AccountInfo(firstname.getText().toString(),lastname.getText().toString(),address.getText().toString(),city.getText().toString(),
+                            country.getText().toString(),age.getText().toString(),height.getText().toString(),weight.getText().toString());
+                    myRef.setValue(accountInfo);
+                }
+
+            }
+        });
         return view;
     }
 }
