deb0f8e044e4c24d76bd2ee68d4c5a5feb98594e
baonehe <95235910+baonehe@users.noreply.github.com>
2022-06-12 16:55:47 +0700

    fix

--- UDDD_Project/app/src/main/java/com/google/uddd_project/CalculatorFragment.java
+++ UDDD_Project/app/src/main/java/com/google/uddd_project/CalculatorFragment.java
@@ -3,11 +3,21 @@ package com.google.uddd_project;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresPermission;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,12 +65,19 @@ public class CalculatorFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    TextView firstname;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calculator, container, false);
+        firstname=view.findViewById(R.id.name);
+        String iduser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference(iduser);
+        ReadData();
         view.findViewById(R.id.bmi_calculate).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,5 +100,17 @@ public class CalculatorFragment extends Fragment {
             }
         });
         return view;
+    }
+    private  void ReadData(){
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                AccountInfo post = snapshot.getValue(AccountInfo.class);
+                firstname.setText(post.getFirstname());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 }
